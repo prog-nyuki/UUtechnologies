bd93a88098877e2839649ed5438e21fad0fd5b14
prog-nyuki <103023955+prog-nyuki@users.noreply.github.com>
2022-06-29 13:50:42 +0900

    Add files via upload

---
+++ 営業用HP/JS/script.js
@@ -0,0 +1,62 @@
+// Loading
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	easing: 'easeOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 1000,//時間指定(1000＝1秒)
+	strokeWidth: 1.0,//進捗ゲージの太さ
+	color: '#000000',//進捗ゲージのカラー
+	trailWidth: 0.5,//ゲージベースの線の太さ
+	trailColor: '#bbb',//ゲージベースの線のカラー
+	text: {//テキストの形状を直接指定				
+		style: {//天地中央に配置
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-30px 0 0 0',//バーより上に配置
+			transform:'translate(-50%,-50%)',
+			'font-size':'1.25rem',
+			color: '#808080',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});  
+
+// Loading //
+
+
+// 画面遷移アニメーション
+$(window).on('load',function(){
+    $("#splash_text").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+	
+    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    $("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+    
+        $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+	
+    });
+    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    
+   //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+    $('.splashbg').on('animationend', function() {    
+        //この中に動かしたいJSを記載
+    });
+    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+        
+});
+// 画面遷移アニメーション //
+
+// Hamburger Menu
+$(".openbtn").click(function () {
+    $(this).toggleClass('active');
+    $(".sm-gnav").toggleClass('active');
+});
+
+// /Hamburger Menu 
